8dd3aee6cf6db448d6e1194459c5adb055312282
Dominik Schreiber <dominik.schreiber@sap.com>
2025-06-30 11:50:22 +0200

    library-less-solution: implement solution

--- webapp/Component.ts
+++ webapp/Component.ts
@@ -5,19 +5,19 @@ import includeStylesheet from "sap/ui/dom/includeStylesheet";
 
 /** @namespace UI5con2025.libraryless */
 export default class Component extends UIComponent {
-  public static metadata = {
-    interfaces: ["sap.ui.core.IAsyncContentCreation"],
-    manifest: "json",
-  };
+	public static metadata = {
+		interfaces: ["sap.ui.core.IAsyncContentCreation"],
+		manifest: "json",
+	};
 
-  async createContent() {
-    // IRL including the right stylesheet would be done by the library;
-    // also, this assumes that a file for that theme actually exists
-    await includeStylesheet(`themes/${Theming.getTheme()}/library.css`);
+	async createContent() {
+		// IRL including the right stylesheet would be done by the library;
+		// also, this assumes that a file for that theme actually exists
+		await includeStylesheet(`css/skeleton.css`);
 
-    return XMLView.create({
-      viewName: "UI5con2025.libraryless.view.App",
-      id: "app",
-    });
-  }
+		return XMLView.create({
+			viewName: "UI5con2025.libraryless.view.App",
+			id: "app",
+		});
+	}
 }
