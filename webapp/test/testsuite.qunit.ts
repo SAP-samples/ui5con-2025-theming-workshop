aa81fa43fb115608ea6958df125d50d3df72ab71
Dominik Schreiber <dominik.schreiber@sap.com>
2025-06-30 12:04:07 +0200

    no-theming: project that does not have a theming solution

---
+++ webapp/test/testsuite.qunit.ts
@@ -0,0 +1,25 @@
+export default {
+	name: "QUnit test suite for UI5con No Theming",
+	defaults: {
+		qunit: { version: 2 },
+		loader: { paths: { "UI5con2025/notheming": "../" } },
+	},
+	tests: {
+		sap_horizon: {
+			page: "ui5://test-resources/UI5con2025/notheming/Test.qunit.html?sap-theme=sap_horizon",
+			title: "UI5con No Theming sap_horizon tests",
+		},
+		sap_horizon_dark: {
+			page: "ui5://test-resources/UI5con2025/notheming/Test.qunit.html?sap-theme=sap_horizon_dark",
+			title: "UI5con No Theming sap_horizon_dark tests",
+		},
+		sap_horizon_hcb: {
+			page: "ui5://test-resources/UI5con2025/notheming/Test.qunit.html?sap-theme=sap_horizon_hcb",
+			title: "UI5con No Theming sap_horizon_hcb tests",
+		},
+		sap_horizon_hcw: {
+			page: "ui5://test-resources/UI5con2025/notheming/Test.qunit.html?sap-theme=sap_horizon_hcw",
+			title: "UI5con No Theming sap_horizon_hcw tests",
+		},
+	},
+};
