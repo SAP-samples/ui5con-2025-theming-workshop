cc45196e21f1bab5d67193173e9e838a9a224de6
Dominik Schreiber <dominik.schreiber@sap.com>
2025-06-30 12:08:46 +0200

    library-less: project that has a library.less

---
+++ webapp/Component.ts
@@ -0,0 +1,23 @@
+import XMLView from "sap/ui/core/mvc/XMLView";
+import UIComponent from "sap/ui/core/UIComponent";
+import Theming from "sap/ui/core/Theming";
+import includeStylesheet from "sap/ui/dom/includeStylesheet";
+
+/** @namespace UI5con2025.libraryless */
+export default class Component extends UIComponent {
+  public static metadata = {
+    interfaces: ["sap.ui.core.IAsyncContentCreation"],
+    manifest: "json",
+  };
+
+  async createContent() {
+    // IRL including the right stylesheet would be done by the library;
+    // also, this assumes that a file for that theme actually exists
+    await includeStylesheet(`themes/${Theming.getTheme()}/library.css`);
+
+    return XMLView.create({
+      viewName: "UI5con2025.libraryless.view.App",
+      id: "app",
+    });
+  }
+}
